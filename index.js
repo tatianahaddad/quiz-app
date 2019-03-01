9353c5f27c5377fa9fd527f2d685be320d468e84
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-02-28 19:01:31 -0800

    added wireframe files

---
+++ index.js
@@ -0,0 +1,16 @@
+// let the user click to start the quiz
+
+// define the questions and answers
+
+// loop through the questions (set up array of questions, log each to console so you know it works)
+
+  // for each question give them choices
+
+  // let the user submit the answer
+
+  // provide feedback
+
+// show the final results
+
+
+// 
